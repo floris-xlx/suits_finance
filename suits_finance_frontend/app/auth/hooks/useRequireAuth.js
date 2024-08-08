001f291c19e662d2b7cbde358081b5d9e89d686c
Floris <floris@xylex.ai>
2024-08-08 15:35:56 +0200

    chore: Refactor useRequireAuth hook and optimize code

--- suits_finance_frontend/app/auth/hooks/useRequireAuth.js
+++ suits_finance_frontend/app/auth/hooks/useRequireAuth.js
@@ -1,39 +1,21 @@
 import { useEffect, useState } from 'react';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { SetKeyLocalStorage } from '@/app/client/caching/LocalStorageRouter';
 import { GetProfilePicById, UpsertUser } from '@/app/client/supabase/SupabaseUserData';
 import stripNameFromEmail from '@/app/client/hooks/formatting/StripNameFromEmail';
-
-
-// zustand
 import { useUserStore, useLoadingStore } from '@/app/stores/stores';
 
-
 export function useRequireAuth() {
-  // zustand user
-  const {
-    setId,
-    setUsername,
-    setProfilePicture,
-    setProviderType,
-  } = useUserStore();
-  const {
-    setAuthLoading
-  } = useLoadingStore();
-
+  const { setId, setUsername, setProfilePicture, setProviderType } = useUserStore();
+  const { setAuthLoading } = useLoadingStore();
   const [userId, setUserId] = useState(null);
   const [loading, setLoading] = useState(true);
-
   const supabaseClient = createClientComponentClient();
 
   useEffect(() => {
     const checkAuth = async () => {
       try {
-
         const { data: session } = await supabaseClient.auth.getSession();
-
-        if (session.session === null) {
-
+        if (!session.session) {
           setAuthLoading(false);
           return;
         }
@@ -41,45 +23,32 @@ export function useRequireAuth() {
         const user = session.session.user;
         const username = user.user_metadata.full_name || stripNameFromEmail(user.email);
 
-        await UpsertUser(user.id,  user.user_metadata.full_name, user.email, username);
-        
-        
-        // zustand
+        await UpsertUser(user.id, user.user_metadata.full_name, user.email, username);
         setUsername(username);
 
-        // this is for getting pfp when the oauth2 provider doesnt provide it
         const profilePic = await GetProfilePicById(user.id);
-        // zustand
         setProfilePicture(profilePic);
         setProviderType('email');
 
-        setAuthLoading(false);
-
-
         const userId = user.id;
-
-
         setUserId(userId);
-        setAuthLoading(false);
-
-
-        // legacy
-        SetKeyLocalStorage('userId', userId);
-        // zustand
         setId(userId);
-
-        setAuthLoading(false);
       } catch (error) {
         console.error('Error checking auth', error);
+      } finally {
         setAuthLoading(false);
       }
     };
 
     checkAuth();
-  }, []);
+  }, [
+    supabaseClient,
+    setAuthLoading,
+    setId,
+    setProfilePicture,
+    setProviderType,
+    setUsername
+  ]);
 
-  return {
-    loading,
-    userId,
-  };
+  return { loading, userId };
 }
